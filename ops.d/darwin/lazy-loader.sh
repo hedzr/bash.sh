79d354a6894bf34f11d0e043c7cd50756d77c318
hz <hedzrz@gmail.com>
2023-01-26 00:58:51 +0800

    improved lazy-loader to load kebab case func and files

--- ops.d/darwin/lazy-loader.sh
+++ ops.d/darwin/lazy-loader.sh
@@ -1,17 +1,26 @@
 # for zsh
 lazy_loaded=()
 function command_not_found_handler() {
-	local f dir processed=0 cmd=$1 && shift
+	local f f1 dir processed=0 cmd="$1" && shift # a args=()
 	for dir in $HOME/.local/bin $HOME/bin /opt/bin /opt/local/bin $HOME/hack/bin $HOME/.r2env/bin; do
-		local dx="$dir/.zsh/lazy"
-		dbg "dx: $dx"
-		if [ -d $dx ]; then
-			f="$dx/$cmd.sh"
-			if not_in_array $f $kazy_loaded; then
-				if [ -f "$f" ]; then
-					source $f && dbg "yes: $f" && processed=1
-					lazy_loaded+=($f)
-					eval $cmd $@
+		if ! (($processed)); then
+			local dx="$dir/.zsh/lazy"
+			# dbg " dir: $dx, args: $*"
+			if [ -d $dx ]; then
+				f="$dx/$cmd.sh"
+				if not_in_array $f $kazy_loaded; then
+					if [ -f "$f" ]; then
+						source $f && dbg "yes: $f" && processed=1
+					else
+						f1="${f//_/-}"
+						if [ -f "$f1" ]; then
+							source $f1 && dbg "yes: $f ($f1 loaded)" && processed=1
+						fi
+					fi
+					if (($processed)); then
+						lazy_loaded+=($f)
+						eval $cmd "$@"
+					fi
 				fi
 			fi
 		fi
