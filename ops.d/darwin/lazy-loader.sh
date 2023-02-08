4163c5a0d1200a05d127b39f51e2af0af6e4a32a
hz <hedzrz@gmail.com>
2023-02-08 13:00:01 +0800

    update lazy-loader, these codes can work for any zsh/bash under darwin or linux and more.

--- ops.d/darwin/lazy-loader.sh
+++ ops.d/darwin/lazy-loader.sh
@@ -2,29 +2,49 @@
 lazy_loaded=()
 function command_not_found_handler() {
 	local f f1 dir processed=0 cmd="$1" && shift # a args=()
-	for dir in $HOME/.local/bin $HOME/bin /opt/bin /opt/local/bin $HOME/hack/bin $HOME/.r2env/bin; do
-		if ! (($processed)); then
-			local dx="$dir/.zsh/lazy"
-			# dbg " dir: $dx, args: $*"
-			if [ -d $dx ]; then
-				f="$dx/$cmd.sh"
-				if not_in_array $f $kazy_loaded; then
-					if [ -f "$f" ]; then
-						source $f && dbg "yes: $f" && processed=1
-					else
-						f1="${f//_/-}"
-						if [ -f "$f1" ]; then
-							source $f1 && dbg "yes: $f ($f1 loaded)" && processed=1
-						fi
+	local osid="$(osid)" pmid="$(pmid)"
+
+	try_source_in() {
+		local dx="$1"
+		if [ -d $dx ]; then
+			f="$dx/$cmd.sh"
+			if not_in_array $f $lazy_loaded; then
+				if [ -f "$f" ]; then
+					source $f && dbg "yes: $f" && processed=1
+				else
+					f1="${f//_/-}"
+					if [ -f "$f1" ]; then
+						source $f1 && dbg "yes: $f ($f1 loaded)" && processed=1
 					fi
-					if (($processed)); then
-						lazy_loaded+=($f)
-						eval $cmd "$@"
-					fi
+				fi
+				if (($processed)); then
+					lazy_loaded+=($f)
+					eval $cmd "$@"
 				fi
 			fi
 		fi
+	}
+
+	for dir in $HOME/.local/bin $HOME/bin /opt/bin /opt/local/bin $HOME/hack/bin $HOME/.r2env/bin; do
+		if ! (($processed)); then
+			local dx="$dir/.zsh/lazy"
+			if [ -d $dx ]; then
+				# dbg " dir: $dx, args: $*"
+				try_source_in "$dx"
+			fi
+		fi
 	done
+
+	if ! (($processed)); then
+		for dir in "$CD/ops.d" "$CD/ops.d/$osid" "$CD/opd.d/$pmid"; do
+			if ! (($processed)); then
+				if [ -d $dx ]; then
+					try_source_in "$dir/lazy"
+				fi
+			fi
+		done
+	fi
+
 	if (($processed)); then
 		return 0
 	else
