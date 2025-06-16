2d11f13af60ed097164d81df9104bc1b4e8302bd
hz <hedzrz@gmail.com>
2025-06-16 14:56:02 +0800

    bump to v20250616

--- bash.sh
+++ bash.sh
@@ -8,7 +8,7 @@
 #
 # bash.sh:
 #   Standard Template for bash/zsh developing.
-#   Version: v20250609
+#   Version: v20250616
 #   License: MIT
 #   Site: https://github.com/hedzr/bash.sh
 #
@@ -252,7 +252,7 @@ _bash_sh_load_env_files() {
 
 ########################################################
 
-#### HZ Tail BEGIN #### v20250609 ####
+#### HZ Tail BEGIN #### v20250616 ####
 in_debug() { (($DEBUG)); }
 in_provisioning() { (($PROVISIONING)); } ## return exit status as true if $PROVISIONING is not equal to 0
 is_root() { [ "$(id -u)" = "0" ]; }
@@ -1224,7 +1224,7 @@ main_do_sth() {
 	# disabling this logic is still simple by defining HAS_END=1.
 	((${HAS_END:-0})) && { debug_begin && echo -n 'Success!' && debug_end; } || return $result_code # { [ $# -eq 0 ] && :; }
 }
-BASH_SH_VERSION=v20250609
+BASH_SH_VERSION=v20250616
 DEBUG=${DEBUG:-0}
 PROVISIONING=${PROVISIONING:-0}
 # Instantly aliases cannot work in many cases such as conditional
@@ -1253,4 +1253,4 @@ path_in_orb_host "$0" && : || { [ -L "$SCRIPT" ] && debug "$(safety "linked scri
 #       CD=$(cd `dirname "$0"`;pwd)
 # It will open a sub-shell to print the folder name of the running shell-script.
 in_sourcing && _bash_sh_load_import_files || main_do_sth "$@"
-#### HZ Tail END #### v20250609 ####
+#### HZ Tail END #### v20250616 ####
