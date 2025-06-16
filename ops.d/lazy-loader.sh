03dedaa4e041465fe2ec45af7ecceb4f8f6e977f
hz <hedzrz@gmail.com>
2025-06-16 14:46:38 +0800

    improved lazy-loader

--- ops.d/lazy-loader.sh
+++ ops.d/lazy-loader.sh
@@ -1,9 +1,23 @@
+# -*- mode: bash; c-basic-offset: 2; tab-width: 2; indent-tabs-mode: t-*-
+# vi: set ft=bash noet ci pi sts=0 sw=2 ts=2:
+# st:
+#
+#
+
 # for zsh
 lazy_loaded=()
-function command_not_found_handler() {
+if fn_exists command_not_found_handler; then
+	dbg "  ..  unset command_not_found_handler and define a new one for ops.sh/ops"
+	unset command_not_found_handler
+else
+	dbg "  ..  define command_not_found_handler for ops.sh/ops"
+fi
+command_not_found_handler() {
 	local command_not_found_handler_cmd="$1" && shift # a args=()
 	local command_not_found_handler_arg="$@"
 	local command_not_found_handler_processed=0
+
+	dbg "  ..[command_not_found_handler] $command_not_found_handler_cmd + $command_not_found_handler_arg"
 
 	_bash_sh_lazy_try_source_in() {
 		local dx="$1" f f1
@@ -16,11 +30,16 @@ function command_not_found_handler() {
 				elif [ -f "${f}-lazy.sh" ]; then
 					source "${f}-lazy.sh" && dbg "  yes: $f" && command_not_found_handler_processed=1
 				else
-					f1="${f//_/-}"
+					f1="$dx/${command_not_found_handler_cmd//_/-}.sh"
 					if [ -f "$f1" ]; then
 						source $f1 && dbg "  yes: $f ($f1 loaded)" && command_not_found_handler_processed=1
 					elif [ -f "${f1}-lazy.sh" ]; then
 						source "${f1}-lazy.sh" && dbg "  yes: $f (${f1}-lazy.sh loaded)" && command_not_found_handler_processed=1
+					else
+						f1="$dx/${command_not_found_handler_cmd//-/_}.sh"
+						if [ -f "$f1" ]; then
+							source $f1 && dbg "  yes: $f ($f1 loaded)" && command_not_found_handler_processed=1
+						fi
 					fi
 				fi
 				if (($command_not_found_handler_processed)); then
@@ -52,7 +71,7 @@ function command_not_found_handler() {
 				local dx="$dir/lazy"
 				if [ -d $dx ]; then
 					dbg "lazy-loader [2nd]: dir: $dx, cmd: $command_not_found_handler_cmd, args: $command_not_found_handler_arg"
-					_bash_sh_lazy_try_source_in "$dir/lazy"
+					_bash_sh_lazy_try_source_in "$dx"
 				fi
 			fi
 		done
