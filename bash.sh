495b6bf7fb3ca245a197102f50c0c79ed5f31c02
hz <hedzrz@gmail.com>
2026-01-03 15:19:47 +0800

    bump to v20260103
      - improved simple-entry.sh
      - fix `which()`, our script needs its stdout

--- bash.sh
+++ bash.sh
@@ -8,7 +8,7 @@
 #
 # bash.sh:
 #   Standard Template for bash/zsh developing.
-#   Version: v20251123
+#   Version: v20260103
 #   License: MIT
 #   Site: https://github.com/hedzr/bash.sh
 #
@@ -304,7 +304,7 @@ repl_bashsh_block() {
 
 ########################################################
 
-#### HZ Tail BEGIN #### v20251123 ####
+#### HZ Tail BEGIN #### v20260103 ####
 in_debug() { (($DEBUG)); }
 in_provisioning() { (($PROVISIONING)); } ## return exit status as true if $PROVISIONING is not equal to 0
 is_root() { [ "$(id -u)" = "0" ]; }
@@ -1399,7 +1399,7 @@ main_do_sth() {
 	# disabling this logic is still simple by defining HAS_END=1.
 	((${HAS_END:-0})) && { debug_begin && echo -n 'Success!' && debug_end; } || return $result_code # { [ $# -eq 0 ] && :; }
 }
-BASH_SH_VERSION=v20251123
+BASH_SH_VERSION=v20260103
 DEBUG=${DEBUG:-0}
 PROVISIONING=${PROVISIONING:-0}
 SUDO=sudo && [ "$(id -u)" = "0" ] && SUDO= || :
@@ -1430,4 +1430,4 @@ path_in_orb_host "$0" && : || { [ -L "$SCRIPT" ] && debug "$(safety "linked scri
 #       CD=$(cd `dirname "$0"`;pwd)
 # It will open a sub-shell to print the folder name of the running shell-script.
 in_sourcing && _bash_sh_load_import_files || main_do_sth "$@"
-#### HZ Tail END #### v20251123 ####
+#### HZ Tail END #### v20260103 ####
