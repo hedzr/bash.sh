3d5efdf5db7714eaed38d653fa701326e9d11200
hz <hedzrz@gmail.com>
2026-02-22 14:27:20 +0800

    bump to v20260222   - fix expanding `$@` args with spaces     - improved `commander`     - improved `lazy-loader`

--- simple-entry.sh
+++ simple-entry.sh
@@ -158,7 +158,7 @@ if_hosttype() { # usage:     if_hosttype x64 && echo x64 || echo x86 | BUT, it o
 # fi
 
 ### final version
-CD="$(cd $(dirname "$0") && pwd)" && BASH_SH_VERSION=v20260131 && DEBUG=${DEBUG:-0} && PROVISIONING=${PROVISIONING:-0}
+CD="$(cd $(dirname "$0") && pwd)" && BASH_SH_VERSION=v20260222 && DEBUG=${DEBUG:-0} && PROVISIONING=${PROVISIONING:-0}
 SUDO=sudo && { [ "$(id -u)" = "0" ] && SUDO= || :; }
 LS_OPT="--color" && { is_darwin && LS_OPT="-G" || :; }
 if (($#)); then
