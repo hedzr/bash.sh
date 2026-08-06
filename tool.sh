317324604f5199f4d0978424b177cfb4e7884bd3
hz <hedzrz@gmail.com>
2026-08-07 05:58:12 +0800

    improved `proxy_set`

--- tool.sh
+++ tool.sh
@@ -27,6 +27,7 @@
 #function ports () { open-ports $*; }
 ports() {
 	local SUDO="${SUDO:-sudo}" && [ "$(id -u)" = "0" ] && SUDO=
+	# set -x
 	if [ $# -eq 0 ]; then
 		if which lsof >/dev/null; then
 			eval $SUDO lsof -Pni | grep -E "LISTEN|UDP"
@@ -47,16 +48,17 @@ ports() {
 		fi
 	else
 		if which lsof >/dev/null; then
-			local p='' i
+			local p='' i prog
 			for i in "$@"; do
-				if [[ "$i" -eq "$i" ]]; then
+				if [[ "$i" =~ '^[0-9]+$' ]]; then
 					p="$p -i :$i"
 				else
-					p="$p -i $i"
+					prog="$i"
 				fi
 			done
-			# debug echo "lsof -Pn $p"
-			eval $SUDO lsof -Pn $p | grep -E "LISTEN|UDP"
+			debug echo "lsof -Pn $p, prog = ${prog//\//|}"
+			[ "$p" != "" ] && tip "listing ports '$p' ..." && eval $SUDO lsof -Pn "$p" | grep -E "LISTEN|UDP" || :
+			[ "$prog" != "" ] && tip "listing programs '${prog//\//|}' ..." && eval $SUDO lsof -Pni | grep -Ei "${prog//\//|}" | grep -v grep || :
 		elif which sockstat >/dev/null; then
 			local p='' i
 			for i in "$@"; do
@@ -141,22 +143,29 @@ proxy_set() {
 	else
 		local pip=$(hostname -I | awk '{print $1}')
 	fi
-	local link=${PROXY_LINK:-http://$pip:7890}
+	local port="${PORT:-7890}"
+	local link="${PROXY_LINK:-http://$pip:$port}"
 
 	clash_proxy_set() {
-		sudo networksetup -setwebproxy "Wi-Fi" 127.0.0.1 7890
-		sudo networksetup -setsecurewebproxy "Wi-Fi" 127.0.0.1 7890
-		sudo networksetup -setsocksfirewallproxy "Wi-Fi" 127.0.0.1 7890
-		sudo networksetup -setproxybypassdomains "Wi-Fi" '192.168.0.0/16,10.0.0.0/8,172.16.0.0/12,100.64.0.0/10,127.0.0.1,127.0.1.1,localhost,*.local,169.254.0.0/16,224.0.0.0/4,240.0.0.0/4,<local>,hz-pc,laoye-mac,loaye16,laoye16,*.test,*.cn.*.crashlytics.com,22os.com,jycode.com,anos.biz,swt.im,yekeke.com,163.com,sina.com.cn,bing.com,sohu.com,*.22os.com,*.jycode.com,*.anos.biz,*.swt.im,*.yekeke.com,*.163.com,*.sina.com.cn,*.bing.com,*.sohu.com,*.zxcs.cme,*.microsoft.com,timestamp.apple.com,sequoia.apple.com,seed-sequoia.siri.apple.com,*.ntp.org,timestamp.windows.com'
+		sudo networksetup -setwebproxy "Wi-Fi" 127.0.0.1 "$port"
+		sudo networksetup -setsecurewebproxy "Wi-Fi" 127.0.0.1 "$port"
+		sudo networksetup -setsocksfirewallproxy "Wi-Fi" 127.0.0.1 "$port"
+		sudo networksetup -setproxybypassdomains "Wi-Fi" '127.0.0.1,192.168.0.0/16,10.0.0.0/8,172.16.0.0/12,100.64.0.0/10,127.0.1.1,localhost,*.local,mbp14,loaye16,laoye16,*.test,169.254.0.0/16,224.0.0.0/4,240.0.0.0/4,*.cn,*.crashlytics.com,22os.com,jycode.com,anos.biz,swt.im,yekeke.com,163.com,sina.com.cn,bing.com,sohu.com,*.22os.com,*.jycode.com,*.anos.biz,*.swt.im,*.yekeke.com,*.163.com,*.sina.com.cn,*.bing.com,*.sohu.com,*.zxcs.cme,*.microsoft.com,timestamp.apple.com,sequoia.apple.com,*.siri.apple.com,*.ntp.org,timestamp.windows.com,<local>'
 	}
 
+	proxy_print_st() { proxy_print_status "$@"; }
+	proxy_print_stat() { proxy_print_status "$@"; }
+	proxy_print_stats() { proxy_print_status "$@"; }
 	proxy_print_status() {
+		tip "The environment variables are:"
 		[ "$http_proxy" != "" ] && echo "http_proxy=$http_proxy"
 		[ "$HTTP_PROXY" != "" ] && echo "HTTP_PROXY=$HTTP_PROXY"
 		[ "$https_proxy" != "" ] && echo "https_proxy=$https_proxy"
 		[ "$HTTPS_PROXY" != "" ] && echo "HTTPS_PROXY=$HTTPS_PROXY"
 		[ "$all_proxy" != "" ] && echo "all_proxy=$all_proxy"
 		[ "$ALL_PROXY" != "" ] && echo "ALL_PROXY=$ALL_PROXY"
+		tip "The system proxy is:"
+		networksetup -getwebproxy "Wi-Fi"
 	}
 	proxy_set_off() {
 		unset all_proxy ALL_PROXY http_proxy https_proxy HTTP_PROXY HTTPS_PROXY
@@ -164,6 +173,14 @@ proxy_set() {
 	proxy_set_on() {
 		export http_proxy="$link"
 		export https_proxy="$http_proxy" HTTPS_PROXY="$http_proxy" HTTP_PROXY="$http_proxy" all_proxy="$http_proxy" ALL_PROXY="$http_proxy"
+	}
+	proxy_set_tog() { proxy_set_toggle "$@"; }
+	proxy_set_toggle() {
+		if [ "$HTTP_PROXY" = "" ]; then
+			proxy_set_on "$@"
+		else
+			proxy_set_off "$@"
+		fi
 	}
 	proxy_set_invoke() {
 		# for better compatibilities under macOS we assumed a child shell for cleanup the envvars.
@@ -185,29 +202,30 @@ proxy_set() {
 	}
 	case $onoff in
 	on | ON | 1 | yes | ok | enable | enabled | open | allow)
-		proxy_set_on
+		proxy_set_on "$@"
 		echo 'HTTP Proxy on (http)'
 		;;
 	off | OFF | 0 | no | bad | disable | disabled | close | disallow | deny)
-		proxy_set_off
+		proxy_set_off "$@"
 		echo 'HTTP Proxy off (http)'
 		;;
-	status | st)
-		proxy_print_status
+	toggle | tog | t) proxy_set_toggle "$@" ;;
+	status | st | stat | stats)
+		proxy_print_status "$@"
 		;;
 	clash)
-		is_darwin && clash_proxy_set || :
+		is_darwin && clash_proxy_set "$@" || :
 		;;
 	usage | help | info)
-		echo 'Usage: proxy_set on|off|enable|disable|allow|deny|status'
-		echo 'Or run proxy_set just like "tsock": proxy_set curl -iL https://google.com/'
-		echo 'Type "proxy_set help" for more information.'
-		echo 'Use envvar if LAN ip not detected: PROXY_LINK=http://xx.xx.xx.xx:xxxx proxy_set on'
+		printf "${clr_italic}Usage: ${clr_reset_all}${clr_bold}proxy_set on|off|enable|disable|allow|deny|status|toggle|tog${clr_reset_all}\n"
+		printf "${clr_italic}Or run proxy_set just like "tsock": ${clr_reset_all}${clr_bold}proxy_set curl -iL https://google.com/${clr_reset_all}\n"
+		printf "${clr_italic}Type ${clr_reset_all}\"${clr_bold}proxy_set help\" ${clr_italic}for more information.${clr_reset_all}\n"
+		printf "${clr_italic}Use envvar if LAN ip not detected: ${clr_reset_all}${clr_bold}PROXY_LINK=http://xx.xx.xx.xx:xxxx proxy_set on${clr_reset_all}\n"
 		echo
-		echo 'For clash verge rev mac, here is a patch to enable it at system proxy settings:'
+		printf "${clr_dim}For clash verge rev mac, here is a patch to enable it at system proxy settings:${clr_reset_all}\n"
 		echo '  proxy_set clash'
 		echo
-		proxy_print_status
+		proxy_print_status "$@"
 		;;
 	*)
 		proxy_set_invoke "$@"
